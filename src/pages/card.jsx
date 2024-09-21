d2fc003be0de08bc6a47dd0fe8167a200628156e
jihannkn <khanzajihan@gmail.com>
2024-09-21 13:34:48 +0700

    pages(card): update card

--- src/pages/card.jsx
+++ src/pages/card.jsx
@@ -1,5 +1,8 @@
-export default function card (){
-    return(
-        <h1>ini halaman card</h1>
+export default function card() {
+    return (
+        <>
+            <h1>ini halaman card</h1>
+            <h2>APA BENAR CARD</h2>
+        </>
     )
 }
